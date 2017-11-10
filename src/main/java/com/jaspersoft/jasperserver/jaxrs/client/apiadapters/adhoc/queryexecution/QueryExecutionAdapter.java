e627e3949d59ec5d6b754d8ee23ab015b8eb292c
TanyaEf <efimenkot@gmail.com>
2017-11-10 10:37:32 +0200

    Revert "deleted redundant method"

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/adhoc/queryexecution/QueryExecutionAdapter.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/apiadapters/adhoc/queryexecution/QueryExecutionAdapter.java
@@ -81,6 +81,11 @@ public class QueryExecutionAdapter extends AbstractAdapter {
         return this;
     }
 
+    public QueryExecutionAdapter withQuerParameter(String name, String value) {
+        params.add(name, value);
+        return this;
+    }
+
     public <T> OperationResult<T> retrieveData(String executionId) {
         uri.add(executionId);
         uri.add("data");
