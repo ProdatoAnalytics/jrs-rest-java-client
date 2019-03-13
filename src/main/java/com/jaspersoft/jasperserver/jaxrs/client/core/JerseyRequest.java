f41ef7faa65e99dbd30b2105219dbfd481dc69fc
Tetyana Matveyeva <tmatveye@tibco.com>
2019-03-13 19:05:42 +0100

    Added acccept language to all requests from session storage

--- src/main/java/com/jaspersoft/jasperserver/jaxrs/client/core/JerseyRequest.java
+++ src/main/java/com/jaspersoft/jasperserver/jaxrs/client/core/JerseyRequest.java
@@ -30,8 +30,7 @@ import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationRe
 import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResultFactory;
 import com.jaspersoft.jasperserver.jaxrs.client.core.operationresult.OperationResultFactoryImpl;
 import org.glassfish.jersey.uri.UriComponent;
-import java.util.List;
-import java.util.Map;
+
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
@@ -40,6 +39,9 @@ import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import static com.jaspersoft.jasperserver.jaxrs.client.core.enums.MimeType.JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -61,16 +63,15 @@ public class JerseyRequest<ResponseType> implements RequestBuilder<ResponseType>
     private String contentType;
     private String acceptType;
     private Boolean handleErrors;
-    private SessionStorage storage;
+    private Locale userLocale;
 
     protected JerseyRequest(SessionStorage sessionStorage, Class<ResponseType> responseClass) {
         operationResultFactory = new OperationResultFactoryImpl();
         this.responseClass = responseClass;
         this.responseGenericType = null;
         restrictedHttpMethods = sessionStorage.getConfiguration().getRestrictedHttpMethods();
-        storage = sessionStorage;
-        init();
-
+        this.userLocale = sessionStorage.getUserLocale();
+        init(sessionStorage);
     }
 
 
@@ -137,7 +138,7 @@ public class JerseyRequest<ResponseType> implements RequestBuilder<ResponseType>
 
     @Override
     public OperationResult<ResponseType> get() throws JSClientWebException {
-        Invocation.Builder request = buildRequest();
+        Invocation.Builder request = buildRequest().acceptLanguage(userLocale);
         return executeRequest(GET, request);
     }
 
@@ -149,13 +150,13 @@ public class JerseyRequest<ResponseType> implements RequestBuilder<ResponseType>
 
     @Override
     public OperationResult<ResponseType> put(Object entity) throws JSClientWebException {
-        Invocation.Builder request = buildRequest();
+        Invocation.Builder request = buildRequest().acceptLanguage(userLocale);
         return executeRequest(PUT, request, entity);
     }
 
     @Override
     public OperationResult<ResponseType> post(Object entity) throws JSClientWebException {
-        Invocation.Builder request = buildRequest();
+        Invocation.Builder request = buildRequest().acceptLanguage(userLocale);
         return executeRequest(POST, request, entity);
     }
 
